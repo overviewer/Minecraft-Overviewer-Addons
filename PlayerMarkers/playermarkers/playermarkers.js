fbe8bfcff63302338d90ac0cb345b5e0ca415e5e
Michael Writhe <michael@writhem.com>
2012-03-13 11:38:10 -0600

    Updated MapMarkers to work with Overviewer 0.9+ (anvil/devel)

--- PlayerMarkers/playermarkers/playermarkers.js
+++ PlayerMarkers/playermarkers/playermarkers.js
@@ -1,87 +1,52 @@
 // PlayerMarkers for use with MapMarkers bukkit plugin.
-// Original code by TJ09, modified and cleaned up for use with Overviewer 0.1.3+ by Michael Writhe and Hauke Schade
-// https://github.com/overviewer/Minecraft-Overviewer-Addons
+// Original code by TJ09, modified and cleaned up for use with Overviewer 0.9+ by Michael Writhe
+// Original URL: http://forums.bukkit.org/threads/info-mapmarkers-v0-3-4-1-1r6.843/
+// Current URL: https://github.com/overviewer/Minecraft-Overviewer-Addons
 // Please make sure you have markers.json accessable to the web.
 
 var playerMarkers = null;
-var infoWindowsArray = null;
-var foundPlayerMarkers = null;
+var warpMarkers = [];
 var refreshTime = 15;
-var avatarserver = "http://cerato.writhem.com/player-avatar.php?player=";
+var avatarserver = "http://new.overviewer.org/avatar/";
 
-setInterval(updatePlayerMarkers, 1000 * refreshTime);
-setTimeout(updatePlayerMarkers, 1000);
-
-function prepareInfoWindow(infoWindow, item) {
-	var c = "<div class=\"infoWindow\" style='width: 300px'><img src='"+avatarserver+item.msg+"'/><h1>"+item.msg+"</h1><p style='text-align: left;'>X: "+item.x+"<br />Y: "+item.y+"<br />Z: "+item.z+"</p></div>";
-	if (c != infoWindow.getContent())
-		infoWindow.setContent(c);
+function deletePlayerMarkers() {
+  if (playerMarkers) {
+    for (i in playerMarkers) {
+      playerMarkers[i].setMap(null);
+    }
+    playerMarkers = null;
+  }
 }
 
-function updatePlayerMarkers() {
-	$.getJSON('markers.json?'+Math.round(new Date().getTime()), function(data) {
-		if(playerMarkers == null)
-			playerMarkers = [];
-		if (infoWindowsArray == null)
-			infoWindowsArray = [];
-		var foundPlayerMarkers = [];
-		for (i in playerMarkers)
-			foundPlayerMarkers.push(false);
+setInterval(loadPlayerMarkers, 1000 * refreshTime);
+setTimeout(loadPlayerMarkers, 1000);
 
+function preparePlayerMarker(marker,item) {
+	var c = "<div class=\"infoWindow\" style='width: 300px'><img src=\""+avatarserver+item.msg+"\"><h1>"+item.msg+"</h1></div>";
+	var infowindow = new google.maps.InfoWindow({content: c});
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(overviewer.map,marker);
+	});
+}
+
+function loadPlayerMarkers() {
+	$.getJSON('markers.json', function(data) {
+		deletePlayerMarkers();
+		playerMarkers = [];
 		for (i in data) {
 			var item = data[i];
-			if(item.id != 4) continue; //this is some compatibility
-			var converted = overviewer.util.fromWorldToLatLng(item.x, item.y, item.z);
-
-			//if found, change position
-			var found = false;
-			for (player in playerMarkers) {
-				if(playerMarkers[player].getTitle() == item.msg) {
-					foundPlayerMarkers[player] = found = true;
-					playerMarkers[player].setPosition(converted);
-					if(playerMarkers[player].getMap() == null)
-						playerMarkers[player].setMap(overviewer.map);
-					prepareInfoWindow(infoWindowsArray[player], item);
-					break;
-				}
-			}
-			//elsenew marker
-			if(!found) {
-				var marker =  new google.maps.Marker({
-					position: converted,
-					map: overviewer.map,
-					title: item.msg,
-					icon: avatarserver+item.msg,
-					visible: true,
-					zIndex: 999
-				});
-				playerMarkers.push(marker);
-				var infowindow = new google.maps.InfoWindow({content: item.msg});
-				prepareInfoWindow(infowindow, item);
-				google.maps.event.addListener(marker, 'click', function(event) {
-					var i = 0;
-					for (playerindex in playerMarkers) {
-						if (playerMarkers[playerindex].title == this.title) {
-							i = playerindex;
-							break;
-						}
-					}
-					if(infoWindowsArray[i].getMap()){
-						infoWindowsArray[i].close()
-					} else {
-						infoWindowsArray[i].open(overviewer.map, playerMarkers[i]);
-					}
-				});
-				infoWindowsArray.push(infowindow);
-				foundPlayerMarkers.push(true);
-			}
-		}
-
-		//remove unused markers
-		for (i in playerMarkers) {
-			if (!foundPlayerMarkers[i]) {
-				playerMarkers[i].setMap(null);
-			}
+			if(item.id != 4) continue;
+			var converted = overviewer.util.fromWorldToLatLng(item.x, item.y, item.z, overviewer.mapView.options.currentTileSet);
+			var marker =  new google.maps.Marker({
+				position: converted,
+				map: overviewer.map,
+				title: item.msg,
+				icon: avatarserver+item.msg,
+				visible: true,
+				zIndex: 999
+			});
+			playerMarkers.push(marker);
+			preparePlayerMarker(marker,item);
 		}
 	});
 }
