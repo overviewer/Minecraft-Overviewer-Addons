7260b8bfdcc99751f677a003bef659d96c0cf6b7
Michael Writhe <michael@writhem.com>
2012-07-05 14:00:33 -0600

    Updating URL of centralized avatar server.

--- PlayerMarkers/playermarkers/playermarkers.js
+++ PlayerMarkers/playermarkers/playermarkers.js
@@ -4,7 +4,7 @@
 
 var JSONFile            =    'markers.json'; //The JSON file containing the player data
 var refreshTime         =   5; //How many seconds should we wait between updating the JSONFile.
-var avatarserver        =   'http://new.overviewer.org/avatar/<playername>/head'; //The address for the player avatar script. 
+var avatarserver        =   'http://overviewer.org/avatar/<playername>/head'; //The address for the player avatar script. 
 
 var showPlayerMarkers   =   true; // Should we show the players moving around on the map?
 var playerMarkers       =    []; //The array of player objects
